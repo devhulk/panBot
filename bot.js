ee6dd1ba457bb37e1f315e4b329cd8d456984a7f
Yerden II, Gerald <Gerald.Yerden@duke-energy.com>
2018-04-06 12:41:44 -0400

    mods

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
-const Discord = require('discord.io');
-const client = new Discord.client()
-
-client.on('ready', () => {
-  console.log('I am ready!')
-})
-
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong')
-  }
-})
-
-client.login(process.env.BOT_TOKEN)
-
+var Discord = require('discord.io');
+ 
+var bot = new Discord.Client({
+    token: "NDMxODUwNjM1OTI3ODc5Njkw.DakzGQ.G3exkP65rOHTIca20-4OQQV1i8k",
+    autorun: true
+});
+ 
+bot.on('ready', function() {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+ 
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
